c15b6f3dc43332ee297e41fd8bc75cdeb3c79a01
MariaBir <mariabiryukova3108@gmail.com>
2025-01-29 12:45:30 +0500

    rename

---
+++ lab3_/3.py
@@ -0,0 +1,10 @@
+
+# x + y = numheads
+# 2x + 4y = numlegs 
+def solve(numheads, numlegs ):
+    a = (4*numheads - numlegs)//2
+    b = numheads - a
+    return f"Chickens: {a}, Rabbits: {b}"
+
+
+print(solve(35, 94))
