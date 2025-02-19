e72c9aed5763949a4c512ccbef8bd7328f09d467
MariaBir <mariabiryukova3108@gmail.com>
2025-02-19 12:33:17 +0500

    Lab5 practice

---
+++ Lab5/3.py
@@ -0,0 +1,5 @@
+import re
+with open("/Users/maria/Desktop/pp2/Lab5/row.txt", "r") as file:
+    txt = file.read()
+x = re.findall(r"\b[a-z]+_[a-z]+\b", txt)
+print(x)
---
+++ Lab5/5.py
@@ -0,0 +1,5 @@
+import re
+with open("/Users/maria/Desktop/pp2/Lab5/row.txt", "r") as file:
+    txt = file.read()
+x = re.findall("a.*b", txt)
+print(x)
